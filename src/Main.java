1be5b7997110f95efa454782fb86fbc7ee4fd56c
bluediamondgamers <djhenry94@gmail.com>
2017-09-17 20:08:06 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Insecure: Issa Hive vs. Lawrence Hive");
+
+        int teamIssa = 0;
+        int teamLawrence = 0;
+        String myAnswer;
+
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Team Issa Score: " + teamIssa);
+        System.out.println("Team Lawrence Score: " + teamLawrence);
+
+        System.out.println("Lets play!");
+
+        System.out.println("Was Issa wrong for cheating on Lawrence? (Y/N)");
+        myAnswer=keyboard.next();
+
+        if(myAnswer =="Y"){
+            teamLawrence= teamLawrence + 1;
+
+        }else{
+            teamIssa = teamIssa +1;
+        }
+
+        System.out.println("Did Issa deserve to be called a hoe ?");
+        myAnswer=keyboard.next();
+
+        if(myAnswer =="N"){
+            teamIssa = teamIssa +1;
+
+        }else{
+            teamIssa = teamIssa +1;
+            teamLawrence= teamLawrence + 1;
+        }
+
+        System.out.println("Did his joblessness make him depressed, and sap his initiative and his " +
+                "willingness to put any effort into being a good boyfriend?");
+        myAnswer=keyboard.next();
+
+        if(myAnswer =="Y"){
+            teamIssa = teamIssa +1;
+
+        }else{
+            teamLawrence= teamLawrence + 1;
+        }
+    }
+
+}
